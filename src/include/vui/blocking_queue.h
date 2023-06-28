20e3d96c1007d185b62ef9a6708c1defba71371a
Josiah Ebhomenye <joebhomenye@gmail.com>
2023-06-28 18:40:39 +0100

    removed shared_ptr from blocking_queue

--- src/include/vui/blocking_queue.h
+++ src/include/vui/blocking_queue.h
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <cstddef>
-#include <memory>
 #include <array>
 #include <optional>
 #include <mutex>
 #include <condition_variable>
+#include <optional>
 
-#define QUEUE_IS_FULL ((m_size + 1) >= Capacity)
+#define QUEUE_IS_FULL (m_size >= Capacity)
 #define QUEUE_IS_EMPTY (m_size <= 0)
 
 namespace vui {
@@ -17,46 +17,56 @@ namespace vui {
     public:
         using entry_type = Entry;
         
-        BlockingQueue();
+        BlockingQueue() = default;
 
-        ~BlockingQueue();
+        ~BlockingQueue() = default;
 
-        bool offer(std::unique_ptr<entry_type> element);
+        BlockingQueue(const BlockingQueue<Entry, Capacity>& other) = delete;
 
-        void put(std::unique_ptr<entry_type> element);
+        BlockingQueue(BlockingQueue<Entry, Capacity>&& other) noexcept {
+            operator=(other);
+        }
 
-        std::unique_ptr<entry_type> take();
+        BlockingQueue& operator=(const BlockingQueue<Entry, Capacity>& other) = delete;
 
-        std::unique_ptr<entry_type> poll();
+        BlockingQueue& operator=(BlockingQueue<Entry, Capacity> other){
+            if(&other == this){
+                return *this;
+            }
+            std::exchange(m_size, other.m_size);
+            std::exchange(m_mutex, other.m_mutex);
+            std::exchange(m_queue_empty_cv, other.m_queue_empty_cv);
+            std::exchange(m_queue_full_cv, other.m_queue_full_cv);
+            std::exchange(m_data, other.m_data);
+
+            return *this;
+        }
+
+        bool offer(entry_type entry);
+
+        void put(entry_type entry);
+
+        entry_type take();
+
+        std::optional<entry_type> poll();
 
     private:
         size_t m_size{0};
         std::mutex m_mutex;
-        std::unique_lock<std::mutex> m_lock;
         std::condition_variable m_queue_empty_cv;
         std::condition_variable m_queue_full_cv;
-        std::array<std::unique_ptr<entry_type>, Capacity> m_data{};
+        std::array<entry_type, Capacity> m_data{};
 
     };
 
     template<typename Entry, size_t Capacity>
-    BlockingQueue<Entry, Capacity>::BlockingQueue() {
-        m_lock = std::unique_lock<std::mutex>(m_mutex);
-    }
-
-    template<typename Entry, size_t Capacity>
-    BlockingQueue<Entry, Capacity>::~BlockingQueue(){
-        m_lock.unlock();
-    }
-
-    template<typename Entry, size_t Capacity>
-    bool BlockingQueue<Entry, Capacity>::offer(std::unique_ptr<entry_type> command) {
+    bool BlockingQueue<Entry, Capacity>::offer(entry_type entry) {
         {
             std::lock_guard<std::mutex> llk_guard{m_mutex};
             if (QUEUE_IS_FULL) {
                 return false;
             }
-            m_data[m_size] = std::move(command);
+            m_data[m_size] = entry;
             m_size++;
         }
         m_queue_empty_cv.notify_all();
@@ -64,38 +74,38 @@ namespace vui {
     }
 
     template<typename Entry, size_t Capacity>
-    void BlockingQueue<Entry, Capacity>::put(std::unique_ptr<entry_type> command) {
-        m_lock.lock();
+    void BlockingQueue<Entry, Capacity>::put(entry_type entry) {
+        std::unique_lock<std::mutex> lock{m_mutex};
         if(QUEUE_IS_FULL){
-            m_queue_full_cv.wait(m_lock);
+            m_queue_full_cv.wait(lock);
         }
-        m_data[m_size] = std::move(command);
+        m_data[m_size] = entry;
         m_size++;
-        m_lock.unlock();
+        lock.unlock();
         m_queue_empty_cv.notify_all();
     }
 
     template<typename Entry, size_t Capacity>
-    std::unique_ptr<Entry> BlockingQueue<Entry, Capacity>::take() {
-        m_lock.lock();
+    Entry BlockingQueue<Entry, Capacity>::take() {
+        std::unique_lock<std::mutex> lock{m_mutex};
         if(QUEUE_IS_EMPTY){
-            m_queue_empty_cv.wait(m_lock);
+            m_queue_empty_cv.wait(lock);
         }
         m_size--;
         auto data = std::move(m_data[m_size]);
-        m_lock.unlock();
+        lock.unlock();
         m_queue_full_cv.notify_all();
 
         return data;
     }
 
     template<typename Entry, size_t Capacity>
-    std::unique_ptr<Entry> BlockingQueue<Entry, Capacity>::poll() {
-        std::unique_ptr<entry_type> data;
+    std::optional<Entry> BlockingQueue<Entry, Capacity>::poll() {
+        entry_type data;
         {
             std::lock_guard<std::mutex> llk_guard{m_mutex};
             if (QUEUE_IS_EMPTY) {
-                return nullptr;
+                return {};
             }
             m_size--;
             data = std::move(m_data[m_size]);
